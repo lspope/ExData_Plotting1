e994694cfeeb835fa54b427416ed6887e6404e31
L. S. Pope <lpope.at.gmu@gmail.com>
2015-05-10 14:41:55 -0400

    Update plot3.R
    corrected symbol used in legend

--- plot3.R
+++ plot3.R
@@ -26,6 +26,6 @@ powersubset$datetime <- strptime(powersubset$datetime, format="%d/%m/%Y %H:%M:%S
 plot(powersubset$datetime, powersubset$Sub_metering_1, type="l",  xlab="", ylab="Energy sub metering") 
 points(powersubset$datetime, powersubset$Sub_metering_2, type="l",col="red")
 points(powersubset$datetime, powersubset$Sub_metering_3, type="l",col="blue")
-legend("topright", pch=1, cex=0.5, col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch="_", cex=0.5, col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", width=480,height=480)
 dev.off()
